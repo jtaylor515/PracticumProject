7a9b62e1a5b8033b3ac4bad8a21aa80225618c77
Gabriel Mendonca <gmunhoz@Gabriels-MacBook-Pro.local>
2024-02-24 19:45:58 -0500

    Implemented signup/login/logout logic and restricted access to different web pages + changing nav bar based on authentication status.

--- Code/Flask_V2/website/models.py
+++ Code/Flask_V2/website/models.py
@@ -1,8 +1,10 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Colum(db.String(150))
-    firstName = db.Colum(db.String(150))
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
--- Code/Flask_V2/website/views.py
+++ Code/Flask_V2/website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
 
@@ -7,9 +8,11 @@ def home():
     return render_template("home.html")
 
 @views.route('/dashboard')
+@login_required
 def dashboard():
     return render_template("dashboard.html")
 
 @views.route('/shoe-search')
+@login_required
 def shoe_search():
     return render_template("shoe_search.html")
