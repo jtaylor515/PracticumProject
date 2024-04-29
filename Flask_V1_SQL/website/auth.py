a36bf1a55a234cce299e14440d437cd3bd6cf27e
Gabriel Mendonca <gmunhoz@Gabriels-MacBook-Pro.local>
2024-04-29 11:13:48 -0400

    corrected login

--- Flask_V1_SQL/website/auth.py
+++ Flask_V1_SQL/website/auth.py
@@ -18,7 +18,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.restaurant_search'))
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -59,6 +59,6 @@ def sign_up():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.restaurant_search'))
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
